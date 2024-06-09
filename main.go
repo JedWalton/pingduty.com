f370159b449dd5e7c95e6fbd723f7cfa84e0b419
Jed Walton <jedwaltondev@gmail.com>
2024-06-09 20:48:24 +0100

    +

--- main.go
+++ main.go
@@ -125,5 +125,23 @@ func notifyDiscord(message string, discordWebhookURL string) {
 }
 
 func main() {
+	frontend()
 	pingduty()
+}
+
+func frontend() {
+	// Serve static assets like CSS, JS, and images
+	fs := http.FileServer(http.Dir("./frontend")) // Serves the current directory
+
+	// Handle all requests by serving a file of the same name
+	http.Handle("/", fs)
+
+	// Log that the server is starting
+	log.Println("Listening on :8080...")
+
+	// Start the server on port 8080
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
